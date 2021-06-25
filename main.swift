5722163c109b42de92914c3f39c1c4cff0786e90
expam99 <54114696+expam99@users.noreply.github.com>
2021-06-25 17:54:48 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,39 @@
+ // con estuctura //
+struct pilita{
+  var arre: [String] = []
+   mutating func agrega(_ elemento: String){
+    arre.append(elemento)
+  }
+  mutating func pop() -> String? {
+    return arre.popLast()
+  }
+  func peek() -> String? {
+    return arre.last
+  }
+}
+
+extension pilita: CustomStringConvertible {
+    
+    var description: String {
+        
+        let topDivider = "\n----------------  mi pila XD  ----------------\n\n"
+        let bottomDivider = "\n\n-----------------------------------------\n"
+        
+        let stackElements = arre.reversed().joined(separator: "\n - ")
+        
+        return topDivider + " - " + stackElements + bottomDivider
+        
+    } 
+
+} 
+
+var pilaReal = pilita()
+
+for i in 1...2 {
+  print("ingresa el elemento " )
+  let j = readLine()!
+  pilaReal.agrega(j)
+}
+
+pilaReal.agrega("hola")
+print(pilaReal)
